90a3e7d7c27eec5dbf11d25345065891bf59ea69
Ben Booth <bkbooth@gmail.com>
2015-10-13 01:13:44 +1100

    Improve build process
    * Use asset revision hashes
    * Add source maps
    * Simplified watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
+var del = require('del');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var gulpif = require('gulp-if');
-var streamify = require('gulp-streamify');
+var rev = require('gulp-rev');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var minifycss = require('gulp-minify-css');
+var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var babelify = require('babelify');
 var browserify = require('browserify');
-var watchify = require('watchify');
+var minifyify = require('minifyify');
 var uglify = require('gulp-uglify');
 
 var production = process.env.NODE_ENV === 'production';
+var jsDir = 'public/js';
+var cssDir = 'public/css';
+var fontsDir = 'public/fonts';
+var assetsDir = 'public/assets';
+var manifestFile = 'public/assets/rev-manifest.json';
 
 var dependencies = [
   'alt',
@@ -28,13 +35,18 @@ var dependencies = [
  | Combine all JS libraries into a single file for fewer HTTP requests.
  |--------------------------------------------------------------------------
  */
-gulp.task('vendor', function() {
+gulp.task('vendor', ['clean'], function() {
   return gulp.src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-  ]).pipe(concat('vendor.js'))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest('public/js'));
+  ]).pipe(sourcemaps.init())
+      .pipe(concat({ path: 'vendor.js', cwd: '.' }))
+      .pipe(gulpif(production, uglify()))
+      .pipe(rev())
+    .pipe(sourcemaps.write('../assets'))
+    .pipe(gulp.dest(jsDir))
+    .pipe(rev.manifest(manifestFile, { base: assetsDir, merge: true }))
+    .pipe(gulp.dest(assetsDir));
 });
 
 /*
@@ -43,12 +55,21 @@ gulp.task('vendor', function() {
  |--------------------------------------------------------------------------
  */
 gulp.task('browserify-vendor', function() {
-  return browserify()
+  var sourceMap = assetsDir + '/vendor.js.map';
+
+  return browserify({ debug: true })
     .require(dependencies)
+    .plugin('minifyify', { map: sourceMap, output: sourceMap })
     .bundle()
     .pipe(source('vendor.bundle.js'))
-    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
-    .pipe(gulp.dest('public/js'));
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(gulpif(production, uglify()))
+      .pipe(rev())
+    .pipe(sourcemaps.write('../assets'))
+    .pipe(gulp.dest(jsDir))
+    .pipe(rev.manifest(manifestFile, { base: assetsDir, merge: true }))
+    .pipe(gulp.dest(assetsDir));
 });
 
 /*
@@ -56,40 +77,25 @@ gulp.task('browserify-vendor', function() {
  | Compile only project files, excluding all third-party dependencies.
  |--------------------------------------------------------------------------
  */
-gulp.task('browserify', ['browserify-vendor'], function() {
-  return browserify('app/main.js')
+gulp.task('browserify', ['clean-app'], function() {
+  var sourceMap = assetsDir + '/app.js.map';
+
+  return browserify('app/main.js', { debug: true })
     .external(dependencies)
     .transform(babelify)
+    .plugin('minifyify', { map: sourceMap, output: sourceMap })
     .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
-    .pipe(gulp.dest('public/js'));
-});
-
-/*
- |--------------------------------------------------------------------------
- | Same as browserify task, but will also watch for changes and re-compile.
- |--------------------------------------------------------------------------
- */
-gulp.task('browserify-watch', ['browserify-vendor'], function() {
-  var bundler = watchify(browserify('app/main.js', watchify.args));
-  bundler.external(dependencies);
-  bundler.transform(babelify);
-  bundler.on('update', rebundle);
-  return rebundle();
-
-  function rebundle() {
-    var start = Date.now();
-    return bundler.bundle()
-      .on('error', function(err) {
-        gutil.log(gutil.colors.red(err.toString()));
-      })
-      .on('end', function() {
-        gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
-      })
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('public/js/'));
-  }
+    .on('error', swallowError)
+    .pipe(source('app.bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(gulpif(production, uglify()))
+      .on('error', swallowError)
+      .pipe(rev())
+    .pipe(sourcemaps.write('../assets'))
+    .pipe(gulp.dest(jsDir))
+    .pipe(rev.manifest(manifestFile, { base: assetsDir, merge: true }))
+    .pipe(gulp.dest(assetsDir));
 });
 
 /*
@@ -97,15 +103,19 @@ gulp.task('browserify-watch', ['browserify-vendor'], function() {
  | Compile SASS stylesheets.
  |--------------------------------------------------------------------------
  */
-gulp.task('styles', function() {
-  return gulp.src('app/stylesheets/main.scss')
+gulp.task('styles', ['clean-styles'], function() {
+  return gulp.src('app/stylesheets/main.scss', { base: 'app/stylesheets' })
     .pipe(plumber())
     .pipe(sourcemaps.init())
       .pipe(sass({ includePaths: ['node_modules'] }))
+      .on('error', swallowError)
       .pipe(autoprefixer())
-      .pipe(gulpif(production, minifycss()))
-    .pipe(sourcemaps.write('../maps'))
-    .pipe(gulp.dest('public/css'));
+      .pipe(gulpif(production, minifyCss()))
+      .pipe(rev())
+    .pipe(sourcemaps.write('../assets'))
+    .pipe(gulp.dest(cssDir))
+    .pipe(rev.manifest(manifestFile, { base: assetsDir, merge: true }))
+    .pipe(gulp.dest(assetsDir));
 });
 
 /*
@@ -115,12 +125,43 @@ gulp.task('styles', function() {
  */
 gulp.task('fonts', function() {
   return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('public/fonts'))
+    .pipe(gulp.dest(fontsDir));
 });
 
-gulp.task('watch', function() {
+/*
+ |--------------------------------------------------------------------------
+ | Clean public directories.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('clean-styles', function() {
+  return del([
+    cssDir + '/*',
+    assetsDir + '/styles*'
+  ]);
+});
+gulp.task('clean-app', function() {
+  return del([
+    jsDir + '/app*',
+    assetsDir + '/app*'
+  ]);
+});
+gulp.task('clean', ['clean-styles'], function() {
+  return del([
+    jsDir + '/*',
+    fontsDir + '/*',
+    assetsDir + '/*'
+  ]);
+});
+
+gulp.task('watch', ['build'], function() {
   gulp.watch('app/stylesheets/**/*.scss', ['styles']);
+  gulp.watch('app/**/*.js', ['browserify']);
 });
 
-gulp.task('default', ['fonts', 'styles', 'vendor', 'browserify-watch', 'watch']);
-gulp.task('build', ['fonts', 'styles', 'vendor', 'browserify']);
+gulp.task('build', ['clean', 'fonts', 'styles', 'vendor', 'browserify-vendor', 'browserify']);
+gulp.task('default', ['build', 'watch']);
+
+function swallowError(error) {
+  gutil.log(error.message, error.stack);
+  this.emit('end');
+}
--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var http = require('http');
 var path = require('path');
 var express = require('express');
 var logger = require('morgan');
-var swig = require('swig');
 
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./app/routes');
+
+var swig = require('swig');
+var fs = require('fs');
+var revManifestFile = 'public/assets/rev-manifest.json';
+swig.setFilter('revManifest', (input) => {
+  var revManifest = JSON.parse(fs.readFileSync(revManifestFile, 'utf8'));
+
+  return revManifest[input] || input;
+});
 
 var app = express();
 
@@ -14,8 +22,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res) {
-  Router.run(routes, req.path, function(Handler) {
+app.use((req, res) => {
+  Router.run(routes, req.path, (Handler) => {
     var html = React.renderToString(React.createElement(Handler));
     var page = swig.renderFile('views/index.html', { html: html });
     res.send(page);
@@ -23,6 +31,6 @@ app.use(function(req, res) {
 });
 
 var server = http.createServer(app);
-server.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), () => {
+  console.info(`Express server listening on port ${app.get('port')}`);
 });
