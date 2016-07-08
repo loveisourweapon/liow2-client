2a83cf6b8129f6a5b1676b95402f99a2c019510f
Ben Booth <bkbooth@gmail.com>
2016-07-08 17:52:57 +1000

    Create 'feed-item' sub-component for feeds

--- app/common/feed/feed.component.js
+++ app/common/feed/feed.component.js
@@ -14,69 +14,13 @@ const FeedComponent = {
         <i class="fa fa-refresh fa-lg"
            ng-class="{ 'fa-spin': $ctrl.loadingFeed }"></i>
       </button>
-    </div>
+    </div><!-- .feed-refresh-button -->
 
     <div class="feed-wrapper">
-      <div class="feed-item clearfix"
-           ng-repeat="item in $ctrl.feedItems track by item._id"
-           in-view="$ctrl.onInView($inview, $index)"
-           in-view-options="{ debounce: 100 }">
-        <div class="feed-item-type">
-          <span class="fa-stack fa-lg">
-            <i class="fa fa-circle fa-stack-2x text-muted"></i>
-            <i class="fa fa-stack-1x fa-inverse"
-               ng-class="{
-                 'fa-heart': item.act,
-                 'fa-quote-right': item.comment && item.target.deed,
-                 'fa-bullhorn': item.comment && item.target.group
-               }"></i>
-          </span>
-        </div>
-
-        <div class="feed-item-image">
-          <img ng-src="{{ ::item.user.picture || $ctrl.User.getDefaultUserImage(item.user._id) }}"
-               class="img-circle">
-        </div>
-
-        <div class="feed-item-content">
-          <h6 class="m-t-none">
-            <a ng-href="/u/{{ ::item.user._id }}"
-               ng-if="$ctrl.User.isLoggedInUser(item.user) || $ctrl.User.isMemberOfGroup(item.group)">
-              {{ ::item.user.firstName }}
-            </a>
-            <span ng-if="!($ctrl.User.isLoggedInUser(item.user) || $ctrl.User.isMemberOfGroup(item.group))">
-              Someone
-            </span>
-
-            <span ng-if="::item.group && !item.target.group">
-              from
-              <a ng-href="/g/{{ ::item.group.urlName }}">{{ ::item.group.name }}</a>
-            </span>
-
-            <span ng-if="::item.act">did the act of love</span>
-            <span ng-if="::item.comment && item.target.deed">left a testimony on</span>
-            <span ng-if="::item.comment && item.target.group">wrote a message to</span>
-
-            <a ng-href="/d/{{ ::item.target.deed.urlTitle }}"
-               ng-if="::item.target.deed">{{ ::item.target.deed.title }}</a>
-
-            <a ng-href="/g/{{ ::item.target.group.urlName }}"
-               ng-if="::item.target.group">{{ ::item.target.group.name }}</a>
-          </h6>
-
-          <div marked="::item.comment.content.text"
-               class="feed-item-comment"
-               ng-if="::item.comment.content.text"></div>
-
-          <p class="small m-b-none">
-            <a href>
-              {{ ::item.created | fromNow }}
-            </a>
-            &middot;
-            {{ ::item.created | moment:'ddd, D MMM YYYY [at] HH:mm' }}
-          </p>
-        </div>
-      </div><!-- .feed-item -->
+      <feed-item ng-repeat="item in $ctrl.feedItems track by item._id"
+                 item="item"
+                 in-view="$ctrl.onInView($inview, $index)"
+                 in-view-options="{ debounce: 100 }"></feed-item>
 
       <div class="feed-item feed-item-narrow text-center"
            ng-show="$ctrl.loading || $ctrl.feedItems === null">
--- app/common/feed/index.js
+++ app/common/feed/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import FeedService from './feed.service';
 import FeedComponent from './feed.component';
+import FeedItemComponent from './feed-item.component';
 
 // Module dependencies
 import angularMarked from 'angular-marked';
@@ -13,6 +14,7 @@ const feed = angular
   ])
   .service('Feed', FeedService)
   .component('feed', FeedComponent)
+  .component('feedItem', FeedItemComponent)
   .name;
 
 export default feed;
