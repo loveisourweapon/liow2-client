16f36541b4139ecddcd0427808427150dc9eb885
Ben Booth <bkbooth@gmail.com>
2016-08-06 00:01:37 +1000

    Fix broken multiple nested transclusion in jumbotron component

--- app/components/jumbotron/jumbotron.controller.js
+++ app/components/jumbotron/jumbotron.controller.js
@@ -1,4 +1,9 @@
 class JumbotronController {
+  /* @ngInject */
+  constructor($element) {
+    Object.assign(this, { $element });
+  }
+
   /**
    * Component is initialised
    */
@@ -6,6 +11,15 @@ class JumbotronController {
     this.styles = {
       'background-image': this.background ? `url(${this.background})` : 'none'
     };
+  }
+
+  /**
+   * All elements compiled and linked
+   */
+  $postLink() {
+    if (this.$element.find('jumbo-content').length) {
+      this.hasJumboContent = true;
+    }
   }
 }
 
