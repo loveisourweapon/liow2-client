144cc08c1a41a1715ccccafb789f0475a05cd886
Ben Booth <bkbooth@gmail.com>
2017-01-19 23:12:28 +1100

    Prevent re-import of CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,14 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, Optional, LOCALE_ID, SkipSelf } from '@angular/core';
 
 @NgModule({
   providers: [
     { provide: LOCALE_ID, useValue: 'en-AU' },
   ],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
