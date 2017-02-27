7b67c7bb421ae08ed0b790a289156508f148be15
Ben Booth <bkbooth@gmail.com>
2017-02-27 17:26:13 +1100

    Remove missing RxJS operators to observables imports

--- src/observables.ts
+++ src/observables.ts
@@ -11,6 +11,8 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/scan';
