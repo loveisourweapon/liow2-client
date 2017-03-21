9487f60d83f11fbdb3b6870fb96cff7b1b266b06
Ben Booth <bkbooth@gmail.com>
2017-03-22 06:57:08 +1100

    Fix generic type usage in takeAndScan testing util

--- src/testing/utils.ts
+++ src/testing/utils.ts
@@ -1,8 +1,9 @@
+import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
-export function takeAndScan(observable: Observable<T>, n: number): Observable<T> {
+export function takeAndScan(observable: Observable<Action>, n: number): Observable<Action[]> {
   return observable
     .take(n)
-    .scan((results: T[], result: T) => [...results, result], [])
+    .scan((results: Action[], result: Action) => [...results, result], [])
     .skip(n - 1);
 }
