6be2e0df4fbb2337540cd1c222e5aac393e2350a
Ben Booth <bkbooth@gmail.com>
2017-03-24 16:20:57 +1100

    Fix unused import

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Event, NavigationEnd, Router } from '@angular/router';
 import { go } from '@ngrx/router-store';
 import { Store } from '@ngrx/store';
