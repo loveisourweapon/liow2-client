352f65ce22148c24be88ec4a0114899fa78a9cf3
Ben Booth <bkbooth@gmail.com>
2017-04-27 11:27:39 +1000

    Use live Facebook client instead of test on test.loveisourweapon.com

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://api-test.loveisourweapon.com',
-  facebookClientId: '1443823562607096',
+  facebookClientId: '1432222363767216',
 };
