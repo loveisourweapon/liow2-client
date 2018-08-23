954046505cc894d1fc8e968007a4e0b63f4afb3b
Ben Booth <bkbooth@gmail.com>
2018-08-23 14:26:01 +1000

    Update test Facebook client ID

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://api-test.loveisourweapon.com',
-  facebookClientId: '1432222363767216',
+  facebookClientId: '283462798922386',
   googleAnalytics: false,
   sentry: true,
 };
