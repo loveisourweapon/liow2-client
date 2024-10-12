c5db677c26bd3d49eb656cfdc18c09868c822f74
Ben Booth <hey@benbooth.dev>
2024-10-12 23:18:26 +1100

    Tweak user image shades

--- src/app/shared/utils/user-image-url.ts
+++ src/app/shared/utils/user-image-url.ts
@@ -5,5 +5,5 @@
  * @param userId a unique id used as the seed for the random user image
  */
 export function getUserImageUrl(userId: string): string {
-  return `https://api.dicebear.com/9.x/thumbs/svg?seed=${userId}&scale=75&shapeColor=ffc4c0,f1f4dc,69d2e7&backgroundColor=a30d02,0b5c83,cd5c14`;
+  return `https://api.dicebear.com/9.x/thumbs/svg?seed=${userId}&scale=75&shapeColor=ffc4c0,f1f4dc,a5cee1&backgroundColor=a30d02,0b5c83,0d6857&eyesColor=000000&mouthColor=000000`;
 }
