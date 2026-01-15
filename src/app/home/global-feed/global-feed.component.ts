9e100a2656b05c5c92d361c7fa250948a0b66b67
Ben Booth <hey@benbooth.dev>
2026-01-15 23:36:34 +1100

    Update primary BeKind header image

--- src/app/home/global-feed/global-feed.component.ts
+++ src/app/home/global-feed/global-feed.component.ts
@@ -1,15 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
-import { StateService, TitleService } from '../../core/services';
+import { EnvironmentService, StateService, TitleService } from '../../core/services';
 
 @Component({
   templateUrl: './global-feed.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GlobalFeedComponent implements OnInit {
-  constructor(
-    private title: TitleService,
-  ) { }
+  constructor(public env: EnvironmentService, private title: TitleService) {}
 
   ngOnInit(): void {
     this.title.set(`Global Activity Feed`);
